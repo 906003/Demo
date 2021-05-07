7a8646831a824bc15b4eacd328340926ae9a12b2
abhiram <abhiram@LAPTOP-HIV2E5DC>
2021-05-07 16:57:14 +0530

    This is a demo repository.

---
+++ src/main/java/com/cts/PracticeDemo1Application.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeDemo1Application.class, args);
+	}
+
+}
